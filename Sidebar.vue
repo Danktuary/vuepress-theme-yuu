c6fbd4c92458a6155ff91e4460b1c5a5e754e069
Sanctuary <sanc.pw@gmail.com>
2018-08-21 13:20:29 -0400

    Lint Sidebar.vue's CSS

--- Sidebar.vue
+++ Sidebar.vue
@@ -77,35 +77,54 @@ export default {
 </script>
 
 <style lang="stylus">
-@import './styles/config.styl'
+@import './styles/config.styl';
 
-.sidebar
-  ul
-    padding 0
-    margin 0
-    list-style-type none
-  a
-    display inline-block
-  .nav-links
-    display none
-    border-bottom 1px solid $borderColor
-    padding 0.5rem 0 0.75rem 0
-    a
-      font-weight 600
-    .nav-item, .repo-link
-      display block
-      line-height 1.25rem
-      font-size 1.1em
-      padding 0.5rem 0 0.5rem 1.5rem
-  .sidebar-links
-    padding 1.5rem 0
+.sidebar {
+	ul {
+		padding: 0;
+		margin: 0;
+		list-style-type: none;
+	}
 
-@media (max-width: $MQMobile)
-  .sidebar
-    .nav-links
-      display block
-      .dropdown-wrapper .nav-dropdown .dropdown-item a.router-link-active::after
-        top calc(1rem - 2px)
-    .sidebar-links
-      padding 1rem 0
+	a {
+		display: inline-block;
+	}
+
+	.nav-links {
+		display: none;
+		border-bottom: 1px solid $borderColor;
+		padding: 0.5rem 0 0.75rem 0;
+
+		a {
+			font-weight: 600;
+		}
+
+		.nav-item, .repo-link {
+			display: block;
+			line-height: 1.25rem;
+			font-size: 1.1em;
+			padding: 0.5rem 0 0.5rem 1.5rem;
+		}
+	}
+
+	.sidebar-links {
+		padding: 1.5rem 0;
+	}
+}
+
+@media (max-width: $MQMobile) {
+	.sidebar {
+		.nav-links {
+			display: block;
+
+			.dropdown-wrapper .nav-dropdown .dropdown-item a.router-link-active::after {
+				top: calc(1rem - 2px);
+			}
+		}
+
+		.sidebar-links {
+			padding: 1rem 0;
+		}
+	}
+}
 </style>
