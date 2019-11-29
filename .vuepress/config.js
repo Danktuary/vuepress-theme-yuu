9fc7d1e579f15c1b8830c61ea10b30bedf3e218b
Sanctuary <sanc.pw@gmail.com>
2019-11-28 23:45:34 -0500

    Install/setup VuePress and vuepress-theme-yuu

---
+++ .vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	title: 'Vuepress Theme Yuu',
+	description: 'A VuePress theme that supports a dark theme, multiple color themes, and other useful features.',
+	theme: 'yuu',
+	themeConfig: {
+		yuu: {
+			defaultDarkTheme: true,
+		},
+		repo: 'danktuary/vuepress-theme-yuu',
+		docsBranch: 'docs',
+		editLinks: true,
+		sidebarDepth: 3,
+		lastUpdated: true,
+	},
+};
