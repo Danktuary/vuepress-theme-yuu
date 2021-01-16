1bfd25e4025dded44dc9645e8d0fd7e00051e511
Sanctuary <sanc.pw@gmail.com>
2021-01-16 01:01:22 -0500

    feat: Install/setup husky and commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	'extends': ['@commitlint/config-conventional'],
+	rules: {
+		'subject-case': [2, 'always', 'sentence-case'],
+		'type-enum': [2, 'always', [
+			'chore',
+			'docs',
+			'feat',
+			'fix',
+			'refactor',
+			'revert',
+			'style',
+			'test',
+		]],
+	},
+}
